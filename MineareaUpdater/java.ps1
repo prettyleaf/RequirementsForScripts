8bd20e0c77954bb9a3d873c6c66567a9801a92b8
bisvi bisvi <81024464+Rockstar234@users.noreply.github.com>
2023-11-21 15:10:26 +0300

    Update java.ps1

--- MineareaUpdater/java.ps1
+++ MineareaUpdater/java.ps1
@@ -1,2 +1,2 @@
-java -jar fabric-installer-0.11.2.jar client -mcversion 1.19.2 -loader 0.14.21 -noprofile
+java -jar fabric-installer-0.11.2.jar client -mcversion 1.19.2 -loader 0.14.24 -noprofile
 pause
